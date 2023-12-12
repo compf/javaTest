98155db2653a6d125d81ecc3d36d74a672596da2
tschoemaker <=>
2023-12-12 21:07:14 +0100

    added intellij files+

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,19 @@
 plugins {
-   application
-    id("org.openrewrite.rewrite") version "6.5.6"
+    id("java")
 }
 
-group = "javaTest"
+group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    mavenLocal()
 }
-
-
-
-application {
-    mainClass.set( "org.example.Main")
-}
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(19))
-    }
-}
-
-
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    rewrite("com.dataClumpFixing:dataClumpFixing:0.1.0-SNAPSHOT")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-rewrite{
-    activeRecipe("com.dataClumpFixing.DataClumpFixer")
-    
-}
-
-
